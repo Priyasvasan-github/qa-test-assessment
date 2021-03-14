230b99d7e1bc119e7861a81a9d800754fe33bffe
Sripriya Srinivasan <sripriyavasan@gmail.com>
2021-03-14 18:51:34 +0100

    Corrections to the existing project for e2e

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,31 +1,55 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
+const reporter = require('cucumber-html-reporter');
+const mkdirp = require('mkdirp')
 
 exports.config = {
   debug: false,
-  allScriptsTimeout: 11000,
+  getPageTimeout: 30 * 1000,
+  allScriptsTimeout: 30 * 1000,
   specs: [
-    './e2e/features/*/*.feature'
+    './e2e/features/*.feature'
   ],
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
-  allScriptsTimeout: 45000,
   baseUrl: 'http://localhost:4200/',
   framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
   cucumberOpts: {
     strict: true,
     require: [
-      './e2e/**/*.steps.ts'
+      './e2e/steps/*.ts',
+      './e2e/hooks/*.ts'
     ],
     format: [
-      'json:test-reports/cucumber-test-results.json'
+      'json:./e2e/test-reports/cucumber-test-results.json'
     ]
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './e2e/tsconfig.e2e.json')
     });
+  },
+  // Create the report folder
+  beforeLaunch: () => {
+    mkdirp('./e2e/test-reports')
+  },
+  // Generate report on completion of run
+  onComplete: () => {
+      var options = {
+        theme: 'bootstrap',
+        jsonFile: './e2e/test-reports/cucumber-test-results.json',
+        output: './e2e/test-reports/cucumber-test-results.html',
+        reportSuiteAsScenarios: true,
+        scenarioTimestamp: true,
+        launchReport: true,
+        metadata: {
+          "Application": "The Star Wars Search",
+          "Browser": "Chrome",
+      }
+    };
+    reporter.generate(options);
   }
 };
